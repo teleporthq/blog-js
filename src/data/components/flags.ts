4ad4c6c0c1d144e8cff2e2351b4d4333062c84f2
Paul <aelythe@gmail.com>
2019-03-13 23:47:21 +0200

    add logos in the footer, fix new component reference

---
+++ src/data/components/flags.ts
@@ -0,0 +1,64 @@
+export function Flags(): ComponentReference {
+  return {
+    type: 'FlagsComponent',
+  }
+}
+
+const FlagsComponent: Component = {
+  name: 'FlagsComponent',
+  content: {
+    name: 'FlagsComponent',
+    type: 'View',
+    source: 'teleport-elements-core',
+    children: [
+      {
+        name: 'View',
+        type: 'View',
+        source: 'teleport-elements-core',
+        style: {
+          margin: '20px 0',
+        },
+        children: [
+          {
+            source: 'teleport-elements-core',
+            name: 'eu',
+            type: 'Image',
+            props: {
+              src: 'https://teleporthq.io/static/img/flags/eu.png',
+              alt: 'European Union Flag',
+            },
+            style: {
+              marginRight: '20px',
+            },
+          },
+          {
+            source: 'teleport-elements-core',
+            name: 'eu',
+            type: 'Image',
+            props: {
+              src: 'https://teleporthq.io/static/img/flags/ro.png',
+              alt: 'European Union Flag',
+            },
+            style: {
+              marginRight: '20px',
+            },
+          },
+          {
+            source: 'teleport-elements-core',
+            name: 'ro',
+            type: 'Image',
+            props: {
+              src: 'https://teleporthq.io/static/img/flags/funds.png',
+              alt: 'European Union Flag',
+            },
+            style: {
+              marginRight: '20px',
+            },
+          },
+        ],
+      },
+    ],
+  },
+}
+
+export default FlagsComponent
